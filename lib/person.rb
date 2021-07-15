ca120e4888b62be92c96ae4bf4e1ef3e3fd282af
garrengoudy <garren.goudy@yahoo.com>
2021-07-15 09:33:31 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+    def initialize(name)
+     @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def job=(the_person_job)
+        @job=the_person_job
+    end
+
+    def job
+        @job
+    end
+ end
